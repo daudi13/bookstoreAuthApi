2a361be157aa3ee825b0448e2d75d87ab9f4c431
daudi13 <davouma13@gmail.com>
2022-08-04 13:29:02 +0300

    refactor methods in book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,18 @@ class BooksController < ApplicationController
 
   # GET /books
   def index
-    @books = Book.all
+    books = User.find(params[:user_id]).books.map do | book |
+      {
+        id: book.id,
+        title: book.title,
+        author: book.author,
+        genre: book.genre,
+        current_chapter: book.current_chapter,
+        total_chapters: book.total_chapters
+      }
+    end
 
-    render json: @books
+    render json: { status: "success", data: books, message: "Books loaded successfull " }, status: :ok
   end
 
   # GET /books/1
@@ -15,27 +24,28 @@ class BooksController < ApplicationController
 
   # POST /books
   def create
-    @book = Book.new(book_params)
+    book = Book.new(book_params)
 
-    if @book.save
-      render json: @book, status: :created, location: @book
+    if book.save
+      render json: { status: "success", data: book, message: "Book created successfully" }, status: :created
     else
-      render json: @book.errors, status: :unprocessable_entity
+      render json: { status: "error", data: book.errors, message: "Book not created" }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /books/1
   def update
     if @book.update(book_params)
-      render json: @book
+      render json: { status: "success", data: @book, message: "Book updated successfully" }, status: :ok
     else
-      render json: @book.errors, status: :unprocessable_entity
+      render json: { status: "error", data: @book.errors, message: "Book not updated" }, status: :unprocessable_entity
     end
   end
 
   # DELETE /books/1
   def destroy
-    @book.destroy
+    @book.destroy!
+    head :no_content
   end
 
   private
@@ -46,6 +56,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.fetch(:book, {})
+      params.require(:book).permit(:user_id, :author, :title, :genre, :current_chapter, :total_chapters)
     end
 end
